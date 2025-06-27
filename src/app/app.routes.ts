ec2165c9edd07f81a56d03b9f85f9193c6fbf153
Ahsan <Ahsan@example.com>
2025-06-27 08:57:07 +0500

    Integrate GithubIntegrationService and configure routing for standalone app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GithubIntegrationComponent } from './components/github-integration/github-integration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: GithubIntegrationComponent },
+];
--- src/app/services/github-integration.service.ts
+++ src/app/services/github-integration.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubIntegrationService {
+  private API_BASE = 'http://localhost:3000/api/github';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStatus(): Observable<{ connected: boolean, username?: string, lastSynced?: string }> {
+    return this.http.get<{ connected: boolean, username?: string, lastSynced?: string }>(
+      `${this.API_BASE}/status`, 
+      { withCredentials: true }
+    );
+  }
+
+  removeIntegration(): Observable<any> {
+    return this.http.post(`${this.API_BASE}/remove`, {}, { withCredentials: true });
+  }
+
+  getData(entity: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.API_BASE}/${entity}`, { withCredentials: true });
+  }
 }
