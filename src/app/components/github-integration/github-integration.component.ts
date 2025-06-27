ba273b5e45ab0900de271a60f0e955dc555cf3e0
Ahsan <Ahsan@example.com>
2025-06-27 08:41:11 +0500

    Initial commit: Angular 19 frontend for GitHub OAuth integration

---
+++ src/app/components/github-integration/github-integration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-integration',
+  imports: [],
+  templateUrl: './github-integration.component.html',
+  styleUrl: './github-integration.component.scss'
+})
+export class GithubIntegrationComponent {
+
+}
